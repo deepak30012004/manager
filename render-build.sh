480d9e9dbee19c711e37d66b2852905d206b88e2
deepak30012004 <136914832+deepak30012004@users.noreply.github.com>
2025-04-14 13:19:41 +0530

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# STEP A: Go into the frontend folder
+cd frontend
+
+# STEP B: Install React packages (like react-scripts)
+npm install
+
+# STEP C: Build the React app
+npm run build
+
+# STEP D: Go back to backend folder
+cd ..
